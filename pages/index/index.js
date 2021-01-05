bd721a31b314dcef88d7e55e65f20c9a04e96368
yangjiang <1084914120@qq.com>
2021-01-05 15:57:50 +0800

    小程序初体验

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,27 @@
 // pages/index/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
+    /**
+    * 页面的初始数据
+    */
+    data: {
+      name: "yjcoder",
+      age: 19,
+      students: [
+          {id: 110, name: "aa", age: 21},
+          {id: 111, name: "bb", age: 22},
+          {id: 112, name: "cc", age: 23},
+      ],
+      counter: 0,
+    },
+    //+counter
+    incrCounter(){
+        let counter = this.data.counter + 1
+        this.setData({
+          counter: counter
+        });
+        console.log(this.data.counter)
+    },
 
   /**
    * 生命周期函数--监听页面加载
