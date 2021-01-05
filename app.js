b544d383d0f761ac7c9474ede9864a4896d83c12
yangjiang <1084914120@qq.com>
2021-01-05 17:47:21 +0800

    注册小程序和页面

--- app.js
+++ app.js
@@ -1,30 +1,65 @@
+//注册小程序示例
 App({
 
   /**
-   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   * 当小程序初始化完成时，执行的函数，这里页面还没生成，会触发 onLaunch（全局只触发一次）
+   * 后台存活2小时
+   * 一般在这里发送网络请求，获取用户信息等
    */
   onLaunch: function () {
-    
+    console.log("初始化完成");
+
+    //异步调用
+    // wx.getUserInfo({
+    //   success: res => {
+    //     console.log(res)
+    //   },
+    // })
+
+    // setTimeout(() => {
+    //   let err = new Error;
+    //   throw err;
+    // }, 3000);
   },
 
   /**
-   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   * 小程序界面显示出来之后会执行的函数，会触发 onShow
    */
   onShow: function (options) {
-    
+
+    //这里获取进入小程序的场景值
+    //console.log(options)
+
+    console.log("页面显示完成");
+
+    //获取用户信息并且将用户信息传递给后台
+    // wx.getUserInfo({
+    //   success: res => {
+    //     console.log(res)
+    //   }
+    // })
+
   },
 
   /**
    * 当小程序从前台进入后台，会触发 onHide
    */
   onHide: function () {
-    
+    console.log("小程序隐藏");
   },
 
   /**
    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
    */
   onError: function (msg) {
-    
-  }
+    console.log("当小程序发生脚本错误，或者 api 调用失败时");
+  },
+
+  /**
+   * 定义全局数据
+  */
+ globalData: {
+   name: "coderWHY"
+ }
+
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,9 @@
 // pages/index/index.js
+//获取全局对象
+// const app = getApp()
+// console.log(app.globalData.name)
+
+//注册页面
 Page({
 
     /**
@@ -13,69 +18,103 @@ Page({
           {id: 112, name: "cc", age: 23},
       ],
       counter: 0,
-    },
-    //+counter
-    incrCounter(){
-        let counter = this.data.counter + 1
-        this.setData({
-          counter: counter
-        });
-        console.log(this.data.counter)
+      eventList: [],
+      pageSize: 30,
+      pageLimit: 1
     },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad(options) {
+    console.log("onLoad")
+    wx.request({
+      url: 'https://api.jiguo.com/wxcode/event/geteventlist',
+      success: (res) => {
+        this.setData({
+          eventList: res.data.result,
+          
+        })
+      },
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
+   * 页面初次渲染完成
    */
-  onReady: function () {
-
+  onReady() {
+    console.log("onReady")
   },
 
   /**
    * 生命周期函数--监听页面显示
+   * 页面显示出来时，执行
    */
-  onShow: function () {
-
+  onShow() {
+    console.log("onShow")
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
+  onHide() {
+    console.log("onHide")
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log("onUnload")
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log("onPullDownRefresh")
   },
 
   /**
    * 页面上拉触底事件的处理函数
+   * 页面滚动到底部
    */
   onReachBottom: function () {
+    console.log("onReachBottom")
+  },
 
+  onPullDownRefresh(){
+    console.log("onPullDownRefresh")
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+    console.log("onShareAppMessage")
+  },
 
-  }
+  //监听页面滚动
+  onPageScroll: (obj) => {
+    console.log(obj)
+  },
+
+  /**
+   * 监听wxml事件
+   */
+  //+counter
+  incrCounter(){
+    let counter = this.data.counter + 1
+    this.setData({
+      counter: counter
+    });
+    console.log(this.data.counter)
+  },
+
+  handleUserInfo: (evt) => {
+    
+    console.log(evt.detail.userInfo)
+  },
+
 })
